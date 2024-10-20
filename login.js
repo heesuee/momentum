c1a826d2d27faf7091a35c0692adfc9a28d4bb54
heesuee <sweetsu25@gmail.com>
2024-10-21 02:09:40 +0900

    add login

--- login.js
+++ login.js
@@ -1,16 +1,17 @@
-const loginForm = document.querySelector('login-form');
-const loginInput = document.querySelector('login-form input');
-const greeting = document.querySelector('greeting');
+const loginForm = document.querySelector('#login-form'); //#은 아이디를 의미, .은 클래스, 아무것도 안붙히면 html태그
+const loginInput = document.querySelector('#login-form input'); 
+const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
 //
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.classList.remove(HIDDEN_CLASSNAME);  
   const username = loginInput.value;
-  greeting.innerText = 'Hello' + usernmae;
-  gretting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = 'Hello ' + username;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+
   // 이거 하면 인풋창에 적고 로그인 버튼 누르면 없어지고 헬로 이름 나와야하는데 왜 안돼...
 }
 
